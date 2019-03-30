af5c837aa8ba725ee82f676a351e9b7c437efc97
Petros Palioudakis <xaniapaliou@gmail.com>
2019-03-30 16:35:43 +0200

    Bug fix on Express App

--- server/index.js
+++ server/index.js
@@ -43,10 +43,10 @@ app.get('/', (req, res) => {
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * FROM values');
 
-  return values.rows;
+    res.send(values.rows);
 });
 
-// get current vallue from Redis
+// get current value from Redis
 app.get('/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
